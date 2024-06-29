2869f5a70562aba84aeb58be20a201fcff5cbfab
jmlc643 <josemarialuyocampos@gmail.com>
2024-06-29 17:28:00 -0500

    first commit

---
+++ src/main/java/com/upao/edu/nutricampusmicroserviciousuario/NutricampusMicroservicioUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.upao.edu.nutricampusmicroserviciousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricampusMicroservicioUsuarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutricampusMicroservicioUsuarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upao/edu/nutricampusmicroserviciousuario/NutricampusMicroservicioUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upao.edu.nutricampusmicroserviciousuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutricampusMicroservicioUsuarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
